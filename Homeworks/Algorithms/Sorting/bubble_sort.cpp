e44c5b26d6361d596d7375426b4558a4d8a6b375
Mikayel5464 <mikayelmurad@gmail.com>
2024-10-09 14:08:00 +0400

    adding bubble sort and selection sort algorithms

---
+++ Homeworks/Algorithms/Sorting/bubble_sort.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+void swap(int&, int&);
+void bubbleSort(int*, size_t);
+void arrayPrint(int*, size_t);
+
+int main() {
+	int array[] = {7, 4, 23, 54, 20, 40, 0};
+	size_t arr_size = sizeof(array) / sizeof(array[0]);
+
+	std::cout << "Not sorted array: ";
+	arrayPrint(array, arr_size);
+
+	bubbleSort(array, arr_size);
+	
+	std::cout << "Sorted array: ";
+	arrayPrint(array, arr_size);
+
+	return 0;
+}
+
+void swap(int& a, int& b) {
+	int tmp = a;
+	a = b;
+	b = tmp;
+}
+
+void bubbleSort(int* arr, size_t size) {
+	for (int i = 0; i < size - 1; ++i) {
+		bool is_swapped = false;
+
+		for (int j = 0; j < size - i - 1; ++j) {
+			if (arr[j] > arr[j + 1]) {
+				swap(arr[j], arr[j + 1]);
+
+				is_swapped = true;
+			}
+		}
+
+		if(!is_swapped) {
+			break;
+		}
+	}
+}
+
+void arrayPrint(int* arr, size_t size) {
+	for (int i = 0; i < size; ++i) {
+		std::cout << arr[i] << " ";
+	}
+
+	std::cout << std::endl;
+}
---
+++ Homeworks/Algorithms/Sorting/selection_sort.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+void swap(int&, int&);
+void arrayPrint(int*, size_t);
+void selectionSort(int*, size_t);
+
+int main() {
+	int array[] = {5, 6, 11, 2, 21, 9};
+	size_t size = sizeof(array) / sizeof(array[0]);
+
+	std::cout << "Not sorted array: ";
+	arrayPrint(array, size);
+
+	selectionSort(array, size);
+
+	std::cout << "Sorted array: ";
+	arrayPrint(array, size);
+	
+	return 0;
+}
+
+void swap(int& a, int& b) {
+	int tmp = a;
+	a = b;
+	b = tmp;
+}
+
+void arrayPrint(int* arr, size_t size) {
+	for (int i = 0; i < size; ++i) {
+		std::cout << arr[i] << " ";
+	}
+
+	std::cout << std::endl;
+}
+
+void selectionSort(int* arr, size_t size) {
+	for (int i = 0; i < size - 1; ++i) {
+		int min_index = i;
+
+		for (int j = i + 1; j < size; ++j) {
+			if (arr[j] < arr[min_index]) {
+				min_index = j;
+			}
+		}
+
+		swap(arr[min_index], arr[i]);
+	}
+}
